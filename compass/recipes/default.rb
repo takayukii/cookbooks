edb7e9df1a720de901d285653a7cd7c9b6f360b9
takayukii <takayukii@gmail.com>
2015-07-30 10:03:29 +0900

    link /usr/local/bin/compass /usr/bin/compass

--- compass/recipes/default.rb
+++ compass/recipes/default.rb
@@ -7,22 +7,14 @@
 # All rights reserved - Do Not Redistribute
 #
 
-data_dir = value_for_platform(
-  "amazon" => { "default" => "/amazon_test" },
-  "ubuntu" => { "default" => "/ubuntu_test" },
-  "default" => "/default_test"
-)
-
-directory data_dir do
-  mode 0755
-  owner 'root'
-  group 'root'
-  recursive true
-  action :create
-end
-
 %w[sass compass].each do |pkg|
   gem_package pkg do
       action :install
   end
 end
+
+link '/usr/local/bin/compass' do
+  to '/usr/bin/compass'
+end
+
+
