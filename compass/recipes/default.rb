6079fd52b434977274cfcfe5997411234c97c79f
takayukii <takayukii@gmail.com>
2015-07-30 09:51:24 +0900

    test recipes

--- compass/recipes/default.rb
+++ compass/recipes/default.rb
@@ -7,6 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
+data_dir = value_for_platform(
+  "amazon" => { "default" => "/amazon_test" },
+  "ubuntu" => { "default" => "/ubuntu_test" },
+  "default" => "/default_test"
+)
+
+directory data_dir do
+  mode 0755
+  owner 'root'
+  group 'root'
+  recursive true
+  action :create
+end
+
 %w[sass compass].each do |pkg|
   gem_package pkg do
       action :install
