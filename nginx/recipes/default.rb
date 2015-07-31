5cd26996ccf04e16a3aafa047430c0afab6d8bf2
takayukii <takayukii@gmail.com>
2015-07-31 13:59:36 +0900

    implements nginx.conf

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -7,24 +7,30 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package "nginx" do
+template '/etc/yum.repos.d/nginx.repo' do
+  mode 0644
+  user 'root'
+  group 'root'
+  path '/etc/yum.repos.d/nginx.repo'
+  source 'nginx.repo.erb'
+end
+
+package 'nginx' do
 	action :install
 end
 
-template "nginx.conf" do
-        path "/etc/nginx/nginx.conf"
-        source "nginx.conf.erb"
-        owner "root"
-        group "root"
-        mode 0644
-        notifies :reload, 'service[nginx]'
+template 'nginx.conf' do
+  path '/etc/nginx/nginx.conf'
+  source 'nginx.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :reload, 'service[nginx]'
 end
 
 
-service "nginx" do
+service 'nginx' do
 	supports :status => true, :restart => true, :reload => true
 	action [ :enable, :start]
 end
-
-
 
