d032b06a53dbf740a79f57a3985fdd02645d78b8
takayukii <takayukii@gmail.com>
2015-07-31 20:38:31 +0900

    add nginx to disable 80

--- nodeproxy/recipes/default.rb
+++ nodeproxy/recipes/default.rb
@@ -15,13 +15,26 @@ template 'nginx.repo' do
   group 'root'
 end
 
+# chkconfig --list (default)
+# nginx 0:off 1:off 2:on  3:on  4:on  5:on  6:off
 package 'nginx' do
 	action :install
+end
+
+template 'nginx.conf' do
+  path '/etc/nginx/nginx.conf'
+  source 'nginx.conf.erb'
+  action :create
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :reload, 'service[nginx]'
 end
 
 template 'nodeproxy.conf' do
   path '/etc/nginx/conf.d/nodeproxy.conf'
   source 'nodeproxy.conf.erb'
+  action :create
   owner 'root'
   group 'root'
   mode 0644
@@ -32,4 +45,3 @@ service 'nginx' do
 	supports :status => true, :restart => true, :reload => true
 	action [ :enable, :start]
 end
-
