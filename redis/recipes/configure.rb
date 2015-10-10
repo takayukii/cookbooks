365c74500272ce634968fc81ab4f45bb7d6f7d89
takayukii <takayukii@gmail.com>
2015-10-11 01:02:58 +0900

    debug chef log

--- redis/recipes/configure.rb
+++ redis/recipes/configure.rb
@@ -1,9 +1,10 @@
-node[:opsworks][:layers].each do |layershortname|
-  node[:opsworks][:layers][layershortname][:instances].each do |instancehostname|
-
+node[:opsworks][:layers].each do |layer|
+  node[:opsworks][:layers][layer][:instances].each do |instancehostname|
+    Chef::Log.debug("-- #{layer} --")
+    Chef::Log.debug("-- #{instancehostname} --")
     bash "test" do
       code <<-EOC
-echo '#{layershortname}#{instancehostname}' >> /test.txt
+echo '#{instancehostname}' >> /test.txt
 EOC
     end
 
