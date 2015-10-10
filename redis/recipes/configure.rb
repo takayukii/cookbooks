354dcbde867d6ebd6093c12dfad959ecba50fcc8
takayukii <takayukii@gmail.com>
2015-10-11 01:35:33 +0900

    debug configure

--- redis/recipes/configure.rb
+++ redis/recipes/configure.rb
@@ -1,10 +1,15 @@
 node[:opsworks][:layers].each do |layer|
   Chef::Log.debug("-- #{layer} --")
+  bash "test" do
+    code <<-EOC
+echo '#{layer}' >> /layer.txt
+EOC
+  end
   layer[:instances].each do |instancehostname|
     Chef::Log.debug("-- #{instancehostname} --")
     bash "test" do
       code <<-EOC
-echo '#{instancehostname}' >> /test.txt
+echo '#{instancehostname}' >> /instance.txt
 EOC
     end
 
