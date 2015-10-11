d5d1c82427b431955987ed35cdfed5cc57d19f6f
takayukii <takayukii@gmail.com>
2015-10-11 15:20:38 +0900

    change crt and key to individual paths

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -6,5 +6,5 @@ default["nginx"][:node_proxy][:phpmyadmin_name] = ""
 default["nginx"][:economy_node_proxy][:http_port] = 80
 default["nginx"][:economy_node_proxy][:https_port] = 500
 default["nginx"][:economy_node_proxy][:phpmyadmin_name] = ""
-default["nginx"][:economy_node_proxy][:crt] = ""
-default["nginx"][:economy_node_proxy][:key] = ""
+default["nginx"][:economy_node_proxy][:crt_path] = "/path/to/cert.crt"
+default["nginx"][:economy_node_proxy][:key_path] = "/path/to/cert.key"
