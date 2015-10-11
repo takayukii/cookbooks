d8a3446ed8022e8bfd9cbd5e38156ae6904275e0
takayukii <takayukii@gmail.com>
2015-10-11 16:01:25 +0900

    fix protocol to https

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -5,7 +5,6 @@ default["nginx"][:node_proxy][:phpmyadmin_name] = ""
 
 default["nginx"][:economy_node_proxy][:http_port] = 80
 default["nginx"][:economy_node_proxy][:https_port] = 500
-default["nginx"][:economy_node_proxy][:node_port] = 443
 default["nginx"][:economy_node_proxy][:phpmyadmin_name] = ""
 default["nginx"][:economy_node_proxy][:crt_path] = "/path/to/cert.crt"
 default["nginx"][:economy_node_proxy][:key_path] = "/path/to/cert.key"
